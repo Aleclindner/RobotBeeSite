5d688dff26c0bb1c8a3bf225eee007a83257a5b4
aleclindner <aleclindner@gmail.com>
2022-09-28 21:58:31 -0700

    initial site setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe src='https://www.youtube.com/embed/GDYOKO6ni-k'
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
